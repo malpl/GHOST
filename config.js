51b18a37c43c7a3d474c4024ad05da1166e76a4a
MALIKCRASH✅️ <168739286+malpl@users.noreply.github.com>
2024-09-21 18:30:58 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put season id",
+SESSION_ID: process.env.SESSION_ID || "tcxC1DiD#PVwZs87akh70Inf_AFSGAoY0oVjUFru8nbOHQWZkNSs",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -94741140620",
-SUDO_NB: process.env.SUDO_NB || "94741140620",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/5hYWrRH/thumb.png",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi GHOST-MD Is Online Now 💻\n*💻 Owner* - GHOST-MD\n\n*💻 Owner Number* -201030825867",
+SUDO_NB: process.env.SUDO_NB || "201030825867",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true"
